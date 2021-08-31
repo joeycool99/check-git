253d2e405c3300bc33cfec6317566cacf79d49b5
joeycool99 <naveenjoe@gmail.com>
2021-08-31 11:06:01 +0530

    First commit

--- app1.js
+++ app1.js
@@ -1,14 +1,100 @@
-var prm = (url) => {
-    return new Promise((response, reject) => {
-        setTimeout(() => {
-            response()
-        }, 1000);
-    })
+// var prm = (url) => {
+//     return new Promise((response, reject) => {
+//         setTimeout(() => {
+//             response()
+//         }, 1000);
+//     })
+
+// }
+
+// prm("wwwbooks/book")
+//     .then(() => {
+//         console.log("done")
+// //     })
+// var a = function (number) {
+//     for (var i = 0; i <= number.length - 1; i++) {
+//         var isPrime = true;
+
+//         if (number[i] === 1) {
+//             console.log("1 is neither prime nor composite number.");
+//         }
+//         else if (number[i] > 1) {
+
+//             for (let j = 2; j < number[i]; j++) {
+//                 if (number[i] % i == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//             if (isPrime) {
+//                 console.log(`${number[i]} is a prime number`);
+//             } else {
+//                 console.log(`${number[i]} is a not prime number`);
+//             }
+//         }
+//     }
+// }
+// a([5, 6, 4, 2, 1])
+
+
+// function check_palindrome(str) {
+//     let j = str.length - 1;
+//     for (let i = 0; i < j / 2; i++) {
+//         let x = str[i];
+//         let y = str[j - i];
+//         if (x != y) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+// v
+
+
+
+
+// function arr(ar) {
+//     for (var i = 0; i <= ar.length - 1; i++) {
+//         var k = ar[i].length - 1
+//         for (var j = 0; j <= k / 2; j++) {
+//             if (ar[i][j] == ar[i][k]) {
+//                 return flase
+//             }
+//         }
+//         return true
+//     }
+// }
+
+
+// var array=["x","w","d","w","x"];  
+// function filterDuplicates(new_array){  
+// return new_array.filter((value,index) =>new_array.indexOf(value)===index);  
+// }  
+// console.log(filterDuplicates(array));  
+
+// const arr = [1, 3, 2, 5, 10];
+// const isPrime = num => {
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) return false;
+//     }
+//     return num !== 1;
+// };
+// let odds = arr.filter(element => isPrime(element));
+// console.log(odds)
+
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
 
 }
 
-prm("wwwbooks/book")
-    .then(() => {
-        console.log("done")
-    })
+Color.prototype.rgb = function () {
+    const { r, g, b } = this
+    return (`rgb(${r},${g},${b})`)
+}
 
+const color1 = new Color(255, 6, 87)
--- script.js
+++ script.js
@@ -1,32 +1,86 @@
 
 
-var request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
-request.send();
-request.onload = function () {
-    var data = JSON.parse(this.response);
-    console.log(data);
-    for (var i = 0; i < 250; i++) {
-        try {
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+// request.send();
+// request.onload = function () {
+//     var data = JSON.parse(this.response);
+//     console.log(data);
+//     for (var i = 0; i < 250; i++) {
+//         try {
 
 
-            var cname = data[i].name;
-            var lang = data[i].latlng;
-            if (lang.length === 0) throw new Error("long is not defined")
-            weatherdata(cname, ...lang)
-        }
-        catch (e) {
-            console.log("error has be handdled" + cname + " " + e.message)
+//             var cname = data[i].name;
+//             var lang = data[i].latlng;
+//             if (lang.length === 0) throw new Error("long is not defined")
+//             weatherdata(cname, ...lang)
+//         }
+//         catch (e) {
+//             console.log("error has be handdled" + cname + " " + e.message)
+//         }
+//     }
+// }
+
+// function weatherdata(name, lat, lang) {
+//     var request = new XMLHttpRequest();
+//     request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lang}&appid=750580de96042e3c2d1b47415f32ca40`, true);
+//     request.send();
+//     request.onload = function () {
+//         var data = JSON.parse(this.response);
+//         console.log(`${name} : ${data.main.temp} `);
+//     }
+// }
+
+
+
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+// request.send();
+// request.onload = function () {
+//     var data = JSON.parse(this.response);
+
+// var but = document.querySelector("button")
+// var ul = document.querySelector("ul")
+// var req = async () => {
+//     var config = {
+//         headers: { Accept: "application/json" }
+//     }
+//     var res = await axios.get('https://icanhazdadjoke.com/', config)
+//     var li = document.createElement("li")
+//     li.innerText = (res.data.joke);
+//     ul.append(li)
+// }
+// but.addEventListener("click", () => req())
+
+
+
+
+const form = document.querySelector("form")
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault()
+    var search = form.elements.query.value
+    var data = await axios.get(`https://api.tvmaze.com/search/shows?q=${search}`)
+    newimg(data.data)
+    form.elements.query.value = ""
+
+})
+
+var newimg = (show) => {
+    for (var img of show) {
+        if (img.show.image) {
+            var image = document.createElement("IMG")
+            image.src = img.show.image.medium
+            document.body.append(image)
         }
     }
 }
 
-function weatherdata(name, lat, lang) {
-    var request = new XMLHttpRequest();
-    request.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lang}&appid=750580de96042e3c2d1b47415f32ca40`, true);
-    request.send();
-    request.onload = function () {
-        var data = JSON.parse(this.response);
-        console.log(`${name} : ${data.main.temp} `);
-    }
+String.prototype.yell = function () {
+    console.log(this.toUpperCase())
+}
+
+
+Array.prototype.pop = function () {
+    console.log("i love you")
 }
